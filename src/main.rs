2fbf40aa820d3fc596686ccace4e9ecd4735a8c9
Chance Linz <clinz.dev@gmail.com>
2023-09-13 17:00:01 -0700

    feat: added clap, basic command line argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use clap::Parser;
+use models::args::CommandLineInterfaceArguments;
+
+mod models;
+
 fn main() {
+    let args = CommandLineInterfaceArguments::parse();
+
+    println!("args = {:?}", args);
+
     println!("Hello, world!");
 }
